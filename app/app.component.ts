690f32203c7b44b92cc2300263cfd9984b64b0f6
Eric Bomblatus <ebomblatus@gmail.com>
2017-01-18 10:21:56 -0800

    add component files, root module, entry point file and SystemJS Config

---
+++ app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class = "container">
+    <h1> First Angular 2 App <small>by Eric Bomblatus</small></h1>
+    <h2> First Angular 2 App </h2>
+    <h3> First Angular 2 App </h3>
+    <h4> First Angular 2 App </h4>
+    <h5> First Angular 2 App </h5>
+    <h6> First Angular 2 App </h6>
+    <p class = "lead">
+      Lomo culpa gluten-free, velit beard PBR&B before they sold out assumenda pop-up stumptown. Distillery blog PBR&B, aliqua hexagon dolore etsy direct trade.
+    </p>
+    <p>
+      Sapiente non swag typewriter sint iPhone, nihil distillery. <mark>Mustache tempor odio</mark>, glossier lo-fi meggings affogato quinoa vaporware. Vice cold-pressed wayfarers vaporware chillwave pabst elit, celiac portland tumeric glossier organic succulents blue bottle. Veniam organic hammock, chia mixtape man bun austin chambray. Esse fashion axe hot chicken cold-pressed jianbing mlkshk quinoa mixtape cred affogato, nisi portland pork belly cornhole.
+    </p>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
